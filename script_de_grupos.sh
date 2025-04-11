1403ad238ec2312be5ee244e9b2aa14e29da2fca
GaSKG01 <135550309+GaSKG01@users.noreply.github.com>
2025-04-11 15:24:39 -0300

    Update script_de_grupos.sh 2

--- script_de_grupos.sh
+++ script_de_grupos.sh
@@ -12,20 +12,20 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
+echo "Criando usuários"
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GGRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GGRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GGRP_ADM
+useradd Ruan -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GGRP_ADM
+useradd Kaique -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GGRP_ADM
+useradd José -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GGRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_VEN
-seradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_VEN
-seradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_VEN
+useradd Ana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_VEN
+seradd Adriele -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_VEN
+seradd Lais -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_VEN
 
-seradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_SEC
-seradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_SEC
-seradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_SEC
-echo "Especificando permissões dos diretórios"
+seradd Richard -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_SEC
+seradd Renata -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_SEC
+seradd Manuel -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -GRP_SEC
+echo "Deifnindo permissões dos diretórios"
 
 
 chown root:GRP_ADM /adm
@@ -37,4 +37,4 @@ chmod 770 /ven
 chmod 770 /adm
 chmod 777 /publico
 
-echo "Fim..."
+echo "Fim"
